7082d34208db7e02fde7c4755995401d5ff8b3af
chikbob <ricarrdo1488@gmail.com>
2024-04-29 15:15:29 +0300

    add: header, footer and index page

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function alertAdd() {
+  alert('Ви можете додати цю сторінку до закладок, натиснувши CTRL + D на клавіатурі.')
+}
+
+function alertTomorrow() {
+  alert('Ця функція ще в розробці.')
+}
