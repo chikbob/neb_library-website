fd15047d5bbdb7c2f51003fda8d8db5a61f96164
chikbob <ricarrdo1488@gmail.com>
2024-04-29 17:53:03 +0300

    add: about and feedback pages

--- js/app.js
+++ js/app.js
@@ -5,3 +5,14 @@ function alertAdd() {
 function alertTomorrow() {
   alert('Ця функція ще в розробці.')
 }
+
+function requestSupport() {
+  let button_checker = document.querySelector('.feedback__checkbox-input').checked
+  console.log(document.getElementById('email').value)
+  if (!button_checker || document.getElementById('email').value.length === 0 || document.getElementById('message').value.length === 0) {
+    alert("Помилка! Введено не вірні данні!")
+  }
+  else {
+    alert("Повідомлення відправлено!")
+  }
+}
